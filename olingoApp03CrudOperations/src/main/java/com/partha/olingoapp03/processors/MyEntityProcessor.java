7c6dd1e67c93775cb03a708cf87c2fce0f3e1609
biswaspa <biswaspa@10.5.71.146>
2019-11-04 14:27:23 +0530

    updating an entity

--- olingoApp03CrudOperations/src/main/java/com/partha/olingoapp03/processors/MyEntityProcessor.java
+++ olingoApp03CrudOperations/src/main/java/com/partha/olingoapp03/processors/MyEntityProcessor.java
@@ -9,6 +9,7 @@ import org.apache.olingo.commons.api.edm.EdmEntitySet;
 import org.apache.olingo.commons.api.edm.EdmEntityType;
 import org.apache.olingo.commons.api.format.ContentType;
 import org.apache.olingo.commons.api.http.HttpHeader;
+import org.apache.olingo.commons.api.http.HttpMethod;
 import org.apache.olingo.commons.api.http.HttpStatusCode;
 import org.apache.olingo.server.api.OData;
 import org.apache.olingo.server.api.ODataApplicationException;
@@ -33,6 +34,8 @@ import org.springframework.stereotype.Component;
 
 import com.partha.olingoapp03.providers.MyEdmProvider;
 import com.partha.olingoapp03.util.MyUtil;
+
+
 
 @Component
 public class MyEntityProcessor implements EntityProcessor{
@@ -80,7 +83,7 @@ public class MyEntityProcessor implements EntityProcessor{
 		response.setHeader(HttpHeader.CONTENT_TYPE, responseFormat.toContentTypeString());
 	}
 
-	
+
 	private Entity readEntityData(EdmEntitySet edmEntitySet, List<UriParameter> keyParams) throws ODataApplicationException {
 
 		EdmEntityType edmEntityType = edmEntitySet.getEntityType();
@@ -106,16 +109,16 @@ public class MyEntityProcessor implements EntityProcessor{
 		}
 	 * */
 	public void createEntity(ODataRequest request, ODataResponse response, UriInfo uriInfo,
-													 ContentType requestFormat, ContentType responseFormat)
-				throws ODataApplicationException, DeserializerException, SerializerException {
-		
-		
-//		// 1. retrieve the Entity Type
-//		List<UriResource> resourcePaths = uriInfo.getUriResourceParts();
-//		// Note: only in our example we can assume that the first segment is the EntitySet
-//		UriResourceEntitySet uriResourceEntitySet = (UriResourceEntitySet) resourcePaths.get(0);
-//		EdmEntitySet edmEntitySet = uriResourceEntitySet.getEntitySet();
-		
+			ContentType requestFormat, ContentType responseFormat)
+					throws ODataApplicationException, DeserializerException, SerializerException {
+
+
+		//		// 1. retrieve the Entity Type
+		//		List<UriResource> resourcePaths = uriInfo.getUriResourceParts();
+		//		// Note: only in our example we can assume that the first segment is the EntitySet
+		//		UriResourceEntitySet uriResourceEntitySet = (UriResourceEntitySet) resourcePaths.get(0);
+		//		EdmEntitySet edmEntitySet = uriResourceEntitySet.getEntitySet();
+
 		// 1. Retrieve the entity type from the URI 
 		EdmEntitySet edmEntitySet = this.util.getEdmEntitySet(uriInfo);
 		EdmEntityType edmEntityType = edmEntitySet.getEntityType();
@@ -128,46 +131,81 @@ public class MyEntityProcessor implements EntityProcessor{
 		Entity requestEntity = result.getEntity();
 		// 2.2 do the creation in backend, which returns the newly created entity
 		Entity createdEntity = createEntityData(edmEntitySet, requestEntity);
-		
+
 		// 3. serialize the response (we have to return the created entity)
 		ContextURL contextUrl = ContextURL.with().entitySet(edmEntitySet).build(); 
 		EntitySerializerOptions options = EntitySerializerOptions.with().contextURL(contextUrl).build(); // expand and select currently not supported 
-		
+
 		ODataSerializer serializer = this.odata.createSerializer(responseFormat);
 		SerializerResult serializedResponse = serializer.entity(serviceMetadata, edmEntityType, createdEntity, options);
-		
+
 		//4. configure the response object
 		response.setContent(serializedResponse.getContent());
 		response.setStatusCode(HttpStatusCode.CREATED.getStatusCode());
 		response.setHeader(HttpHeader.CONTENT_TYPE, responseFormat.toContentTypeString());
 	}
-	
-	
+
+
 	private Entity createEntityData(EdmEntitySet edmEntitySet,Entity entityToCreate){
-		 EdmEntityType edmEntityType = edmEntitySet.getEntityType();
+		EdmEntityType edmEntityType = edmEntitySet.getEntityType();
 
-		    // actually, this is only required if we have more than one Entity Type
-		    if (edmEntityType.getName().equals(MyEdmProvider.ET_CUSTOMER_NAME)) {
-		      return this.util.createCustomer(edmEntityType, entityToCreate);
-		    }
+		// actually, this is only required if we have more than one Entity Type
+		if (edmEntityType.getName().equals(MyEdmProvider.ET_CUSTOMER_NAME)) {
+			return this.util.createCustomer(edmEntityType, entityToCreate);
+		}
 
-		    return null;
+		return null;
 	}
 
 
 
-	@Override
-	public void updateEntity(ODataRequest request, ODataResponse response, UriInfo uriInfo, ContentType requestFormat,
-			ContentType responseFormat) throws ODataApplicationException, ODataLibraryException {
-		// TODO Auto-generated method stub
+	public void updateEntity(ODataRequest request, ODataResponse response, UriInfo uriInfo,
+			ContentType requestFormat, ContentType responseFormat)
+					throws ODataApplicationException, DeserializerException, SerializerException {
+
+		// 1. Retrieve the entity set which belongs to the requested entity 
+		List<UriResource> resourcePaths = uriInfo.getUriResourceParts();
+		// Note: only in our example we can assume that the first segment is the EntitySet
+		UriResourceEntitySet uriResourceEntitySet = (UriResourceEntitySet) resourcePaths.get(0); 
+		EdmEntitySet edmEntitySet = uriResourceEntitySet.getEntitySet();
+		EdmEntityType edmEntityType = edmEntitySet.getEntityType();
+
+		// 2. update the data in backend
+		// 2.1. retrieve the payload from the PUT request for the entity to be updated 
+		InputStream requestInputStream = request.getBody();
+		ODataDeserializer deserializer = this.odata.createDeserializer(requestFormat);
+		DeserializerResult result = deserializer.entity(requestInputStream, edmEntityType);
+		Entity requestEntity = result.getEntity();
 		
+		// 2.2 do the modification in backend
+		List<UriParameter> keyPredicates = uriResourceEntitySet.getKeyPredicates();
+		// Note that this updateEntity()-method is invoked for both PUT or PATCH operations
+		HttpMethod httpMethod = request.getMethod();
+		updateEntityData(edmEntitySet, keyPredicates, requestEntity, httpMethod);
+
+		//3. configure the response object
+		response.setStatusCode(HttpStatusCode.NO_CONTENT.getStatusCode());
 	}
+
+	/**
+	   * This method is invoked for PATCH or PUT requests
+	   * */
+	  private void updateEntityData(EdmEntitySet edmEntitySet, List<UriParameter> keyParams, Entity updateEntity,
+	      HttpMethod httpMethod) throws ODataApplicationException {
+
+	    EdmEntityType edmEntityType = edmEntitySet.getEntityType();
+
+	    // actually, this is only required if we have more than one Entity Type
+	    if (edmEntityType.getName().equals(MyEdmProvider.ET_CUSTOMER_NAME)) {
+	      this.util.updateCustomer(edmEntityType, keyParams, updateEntity, httpMethod);
+	    }
+	  }
 
 	@Override
 	public void deleteEntity(ODataRequest request, ODataResponse response, UriInfo uriInfo)
 			throws ODataApplicationException, ODataLibraryException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- olingoApp03CrudOperations/src/main/java/com/partha/olingoapp03/util/MyUtil.java
+++ olingoApp03CrudOperations/src/main/java/com/partha/olingoapp03/util/MyUtil.java
@@ -10,6 +10,8 @@ import org.apache.olingo.commons.api.data.Property;
 import org.apache.olingo.commons.api.data.ValueType;
 import org.apache.olingo.commons.api.edm.EdmEntitySet;
 import org.apache.olingo.commons.api.edm.EdmEntityType;
+import org.apache.olingo.commons.api.edm.EdmKeyPropertyRef;
+import org.apache.olingo.commons.api.http.HttpMethod;
 import org.apache.olingo.commons.api.http.HttpStatusCode;
 import org.apache.olingo.server.api.ODataApplicationException;
 import org.apache.olingo.server.api.uri.UriInfoResource;
@@ -36,8 +38,8 @@ public class MyUtil {
 		Entity requestedEntity= null;
 		try {
 			requestedEntity = this.jdbcTemplate.queryForObject("select id , name , email from customer where id=?", 
-													new Object[] {Integer.parseInt(keyParams.get(0).getText())} ,
-													new RowMapper<Entity>() {
+					new Object[] {Integer.parseInt(keyParams.get(0).getText())} ,
+					new RowMapper<Entity>() {
 
 				@Override
 				public Entity mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -49,9 +51,9 @@ public class MyUtil {
 					return entity;	
 				}
 			});
-			
+
 			return requestedEntity;
-			
+
 		} catch (DataAccessException | NumberFormatException e) {
 			e.printStackTrace();
 		}
@@ -65,55 +67,160 @@ public class MyUtil {
 
 		return requestedEntity;
 	}
-	
-	
-	 public EdmEntitySet getEdmEntitySet(UriInfoResource uriInfo) throws ODataApplicationException {
 
-		    List<UriResource> resourcePaths = uriInfo.getUriResourceParts();
-		    // To get the entity set we have to interpret all URI segments
-		    if (!(resourcePaths.get(0) instanceof UriResourceEntitySet)) {
-		      // Here we should interpret the whole URI but in this example we do not support navigation so we throw an
-		      // exception
-		      throw new ODataApplicationException("Invalid resource type for first segment.", HttpStatusCode.NOT_IMPLEMENTED
-		          .getStatusCode(), Locale.ENGLISH);
-		    }
 
-		    UriResourceEntitySet uriResource = (UriResourceEntitySet) resourcePaths.get(0);
+	public EdmEntitySet getEdmEntitySet(UriInfoResource uriInfo) throws ODataApplicationException {
 
-		    return uriResource.getEntitySet();
-		  }
+		List<UriResource> resourcePaths = uriInfo.getUriResourceParts();
+		// To get the entity set we have to interpret all URI segments
+		if (!(resourcePaths.get(0) instanceof UriResourceEntitySet)) {
+			// Here we should interpret the whole URI but in this example we do not support navigation so we throw an
+			// exception
+			throw new ODataApplicationException("Invalid resource type for first segment.", HttpStatusCode.NOT_IMPLEMENTED
+					.getStatusCode(), Locale.ENGLISH);
+		}
+
+		UriResourceEntitySet uriResource = (UriResourceEntitySet) resourcePaths.get(0);
+
+		return uriResource.getEntitySet();
+	}
 
 
 	public Entity createCustomer(EdmEntityType edmEntityType, Entity entityToCreate) {
-		
-			//for simplicity i have not kept any check on it
-			String sql="insert  into customer (id,name,email) values (?,?,?)";
-			Object[] args = new Object[] {entityToCreate.getProperty("ID").getValue(),
-							(String)entityToCreate.getProperty("NAME").getValue(),
-							(String)entityToCreate.getProperty("EMAIL").getValue()
-					};
-						
-			this.jdbcTemplate.update(sql, args);
-		    return entityToCreate;
-			
-//		// TODO Auto-generated method stub
-//		// the ID of the newly created product entity is generated automatically
-//	    int newId = 1;
-//	    while (productIdExists(newId)) {
-//	      newId++;
-//	    }
-//
-//	    Property idProperty = entity.getProperty("ID");
-//	    if (idProperty != null) {
-//	      idProperty.setValue(ValueType.PRIMITIVE, Integer.valueOf(newId));
-//	    } else {
-//	      // as of OData v4 spec, the key property can be omitted from the POST request body
-//	      entity.getProperties().add(new Property(null, "ID", ValueType.PRIMITIVE, newId));
-//	    }
-//	    entity.setId(createId("Products", newId));
-//	    this.productList.add(entity);
-//
-//	    return entity;
+
+		//for simplicity i have not kept any check on it
+		String sql="insert  into customer (id,name,email) values (?,?,?)";
+		Object[] args = new Object[] {entityToCreate.getProperty("ID").getValue(),
+				(String)entityToCreate.getProperty("NAME").getValue(),
+				(String)entityToCreate.getProperty("EMAIL").getValue()
+		};
+
+		this.jdbcTemplate.update(sql, args);
+		return entityToCreate;
+
+		//		// TODO Auto-generated method stub
+		//		// the ID of the newly created product entity is generated automatically
+		//	    int newId = 1;
+		//	    while (productIdExists(newId)) {
+		//	      newId++;
+		//	    }
+		//
+		//	    Property idProperty = entity.getProperty("ID");
+		//	    if (idProperty != null) {
+		//	      idProperty.setValue(ValueType.PRIMITIVE, Integer.valueOf(newId));
+		//	    } else {
+		//	      // as of OData v4 spec, the key property can be omitted from the POST request body
+		//	      entity.getProperties().add(new Property(null, "ID", ValueType.PRIMITIVE, newId));
+		//	    }
+		//	    entity.setId(createId("Products", newId));
+		//	    this.productList.add(entity);
+		//
+		//	    return entity;
 	}
+
+
+	public void updateCustomer(EdmEntityType edmEntityType, List<UriParameter> keyParams, Entity entity,
+			HttpMethod httpMethod) throws ODataApplicationException {
+
+		Entity customerEntity = readCustomerData(edmEntityType, keyParams);
+		if (customerEntity == null) {
+			throw new ODataApplicationException("Entity not found", HttpStatusCode.NOT_FOUND.getStatusCode(), Locale.ENGLISH);
+		}
+
+
+		// loop over all properties and replace the values with the values of the given payload
+		// Note: ignoring ComplexType, as we don't have it in our odata model
+		List<Property> existingProperties = customerEntity.getProperties();
+		for (Property existingProp : existingProperties) {
+			String propName = existingProp.getName();
+
+			// ignore the key properties, they aren't updateable
+			if (isKey(edmEntityType, propName)) {
+				continue;
+			}
+
+			Property updateProperty = entity.getProperty(propName);
+			// the request payload might not consider ALL properties, so it can be null
+			if (updateProperty == null) {
+				// if a property has NOT been added to the request payload
+				// depending on the HttpMethod, our behavior is different
+				if (httpMethod.equals(HttpMethod.PATCH)) {
+					// as of the OData spec, in case of PATCH, the existing property is not touched
+					continue; // do nothing
+				} else if (httpMethod.equals(HttpMethod.PUT)) {
+					// as of the OData spec, in case of PUT, the existing property is set to null (or to default value)
+					existingProp.setValue(existingProp.getValueType(), null);
+					continue;
+				}
+			}
+
+			// change the value of the properties
+			existingProp.setValue(existingProp.getValueType(), updateProperty.getValue());
+		}
+
+		//finally persisting the object
+		String sql = "update customer set name=? , email=? where id=?";
+		Object[] args = new Object[] {  customerEntity.getProperty("NAME").getValue(), 
+				customerEntity.getProperty("EMAIL").getValue(), 
+				customerEntity.getProperty("ID").getValue()};
+		try {
+			this.jdbcTemplate.update(sql, args);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+
+
+
+		//		    Entity productEntity = getProduct(edmEntityType, keyParams);
+		//		    if (productEntity == null) {
+		//		      throw new ODataApplicationException("Entity not found", HttpStatusCode.NOT_FOUND.getStatusCode(), Locale.ENGLISH);
+		//		    }
+		//
+		//		    // loop over all properties and replace the values with the values of the given payload
+		//		    // Note: ignoring ComplexType, as we don't have it in our odata model
+		//		    List<Property> existingProperties = productEntity.getProperties();
+		//		    for (Property existingProp : existingProperties) {
+		//		      String propName = existingProp.getName();
+		//
+		//		      // ignore the key properties, they aren't updateable
+		//		      if (isKey(edmEntityType, propName)) {
+		//		        continue;
+		//		      }
+		//
+		//		      Property updateProperty = entity.getProperty(propName);
+		//		      // the request payload might not consider ALL properties, so it can be null
+		//		      if (updateProperty == null) {
+		//		        // if a property has NOT been added to the request payload
+		//		        // depending on the HttpMethod, our behavior is different
+		//		        if (httpMethod.equals(HttpMethod.PATCH)) {
+		//		          // as of the OData spec, in case of PATCH, the existing property is not touched
+		//		          continue; // do nothing
+		//		        } else if (httpMethod.equals(HttpMethod.PUT)) {
+		//		          // as of the OData spec, in case of PUT, the existing property is set to null (or to default value)
+		//		          existingProp.setValue(existingProp.getValueType(), null);
+		//		          continue;
+		//		        }
+		//		      }
+		//
+		//		      // change the value of the properties
+		//		      existingProp.setValue(existingProp.getValueType(), updateProperty.getValue());
+		//		    }
+
+	}
+
+
+	/* HELPER */
+	private boolean isKey(EdmEntityType edmEntityType, String propertyName) {
+		List<EdmKeyPropertyRef> keyPropertyRefs = edmEntityType.getKeyPropertyRefs();
+		for (EdmKeyPropertyRef propRef : keyPropertyRefs) {
+			String keyPropertyName = propRef.getName();
+			if (keyPropertyName.equals(propertyName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 
 }
