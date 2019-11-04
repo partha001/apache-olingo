a067fb7ac5d64a6ccf1c31c12ba161b049bf82d8
biswaspa <biswaspa@10.5.71.146>
2019-11-04 12:08:57 +0530

    create entity related changes

--- olingoApp03CrudOperations/src/main/java/com/partha/olingoapp03/processors/MyEntityProcessor.java
+++ olingoApp03CrudOperations/src/main/java/com/partha/olingoapp03/processors/MyEntityProcessor.java
@@ -16,9 +16,13 @@ import org.apache.olingo.server.api.ODataLibraryException;
 import org.apache.olingo.server.api.ODataRequest;
 import org.apache.olingo.server.api.ODataResponse;
 import org.apache.olingo.server.api.ServiceMetadata;
+import org.apache.olingo.server.api.deserializer.DeserializerException;
+import org.apache.olingo.server.api.deserializer.DeserializerResult;
+import org.apache.olingo.server.api.deserializer.ODataDeserializer;
 import org.apache.olingo.server.api.processor.EntityProcessor;
 import org.apache.olingo.server.api.serializer.EntitySerializerOptions;
 import org.apache.olingo.server.api.serializer.ODataSerializer;
+import org.apache.olingo.server.api.serializer.SerializerException;
 import org.apache.olingo.server.api.serializer.SerializerResult;
 import org.apache.olingo.server.api.uri.UriInfo;
 import org.apache.olingo.server.api.uri.UriParameter;
@@ -89,13 +93,68 @@ public class MyEntityProcessor implements EntityProcessor{
 		return null;
 	}
 
-
-	@Override
-	public void createEntity(ODataRequest request, ODataResponse response, UriInfo uriInfo, ContentType requestFormat,
-			ContentType responseFormat) throws ODataApplicationException, ODataLibraryException {
-		// TODO Auto-generated method stub
+	/*
+	 * Example request:
+	 * 
+	 * POST URL: http://localhost:8088/myapp.svc/Customers
+	 * Header: Content-Type: application/json; odata.metadata=minimal
+	 * Request body:
+	 	{
+			"ID": 10004,
+			"NAME":"partha biswas",
+			"EMAIL":"partha@gmail.com"
+		}
+	 * */
+	public void createEntity(ODataRequest request, ODataResponse response, UriInfo uriInfo,
+													 ContentType requestFormat, ContentType responseFormat)
+				throws ODataApplicationException, DeserializerException, SerializerException {
 		
+		
+//		// 1. retrieve the Entity Type
+//		List<UriResource> resourcePaths = uriInfo.getUriResourceParts();
+//		// Note: only in our example we can assume that the first segment is the EntitySet
+//		UriResourceEntitySet uriResourceEntitySet = (UriResourceEntitySet) resourcePaths.get(0);
+//		EdmEntitySet edmEntitySet = uriResourceEntitySet.getEntitySet();
+		
+		// 1. Retrieve the entity type from the URI 
+		EdmEntitySet edmEntitySet = this.util.getEdmEntitySet(uriInfo);
+		EdmEntityType edmEntityType = edmEntitySet.getEntityType();
+
+		// 2. create the data in backend 
+		// 2.1. retrieve the payload from the POST request for the entity to create and deserialize it
+		InputStream requestInputStream = request.getBody();
+		ODataDeserializer deserializer = this.odata.createDeserializer(requestFormat);
+		DeserializerResult result = deserializer.entity(requestInputStream, edmEntityType);
+		Entity requestEntity = result.getEntity();
+		// 2.2 do the creation in backend, which returns the newly created entity
+		Entity createdEntity = createEntityData(edmEntitySet, requestEntity);
+		
+		// 3. serialize the response (we have to return the created entity)
+		ContextURL contextUrl = ContextURL.with().entitySet(edmEntitySet).build(); 
+		EntitySerializerOptions options = EntitySerializerOptions.with().contextURL(contextUrl).build(); // expand and select currently not supported 
+		
+		ODataSerializer serializer = this.odata.createSerializer(responseFormat);
+		SerializerResult serializedResponse = serializer.entity(serviceMetadata, edmEntityType, createdEntity, options);
+		
+		//4. configure the response object
+		response.setContent(serializedResponse.getContent());
+		response.setStatusCode(HttpStatusCode.CREATED.getStatusCode());
+		response.setHeader(HttpHeader.CONTENT_TYPE, responseFormat.toContentTypeString());
 	}
+	
+	
+	private Entity createEntityData(EdmEntitySet edmEntitySet,Entity entityToCreate){
+		 EdmEntityType edmEntityType = edmEntitySet.getEntityType();
+
+		    // actually, this is only required if we have more than one Entity Type
+		    if (edmEntityType.getName().equals(MyEdmProvider.ET_CUSTOMER_NAME)) {
+		      return this.util.createCustomer(edmEntityType, entityToCreate);
+		    }
+
+		    return null;
+	}
+
+
 
 	@Override
 	public void updateEntity(ODataRequest request, ODataResponse response, UriInfo uriInfo, ContentType requestFormat,
--- olingoApp03CrudOperations/src/main/java/com/partha/olingoapp03/util/MyUtil.java
+++ olingoApp03CrudOperations/src/main/java/com/partha/olingoapp03/util/MyUtil.java
@@ -8,10 +8,14 @@ import java.util.Locale;
 import org.apache.olingo.commons.api.data.Entity;
 import org.apache.olingo.commons.api.data.Property;
 import org.apache.olingo.commons.api.data.ValueType;
+import org.apache.olingo.commons.api.edm.EdmEntitySet;
 import org.apache.olingo.commons.api.edm.EdmEntityType;
 import org.apache.olingo.commons.api.http.HttpStatusCode;
 import org.apache.olingo.server.api.ODataApplicationException;
+import org.apache.olingo.server.api.uri.UriInfoResource;
 import org.apache.olingo.server.api.uri.UriParameter;
+import org.apache.olingo.server.api.uri.UriResource;
+import org.apache.olingo.server.api.uri.UriResourceEntitySet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -60,6 +64,56 @@ public class MyUtil {
 		}
 
 		return requestedEntity;
+	}
+	
+	
+	 public EdmEntitySet getEdmEntitySet(UriInfoResource uriInfo) throws ODataApplicationException {
+
+		    List<UriResource> resourcePaths = uriInfo.getUriResourceParts();
+		    // To get the entity set we have to interpret all URI segments
+		    if (!(resourcePaths.get(0) instanceof UriResourceEntitySet)) {
+		      // Here we should interpret the whole URI but in this example we do not support navigation so we throw an
+		      // exception
+		      throw new ODataApplicationException("Invalid resource type for first segment.", HttpStatusCode.NOT_IMPLEMENTED
+		          .getStatusCode(), Locale.ENGLISH);
+		    }
+
+		    UriResourceEntitySet uriResource = (UriResourceEntitySet) resourcePaths.get(0);
+
+		    return uriResource.getEntitySet();
+		  }
+
+
+	public Entity createCustomer(EdmEntityType edmEntityType, Entity entityToCreate) {
+		
+			//for simplicity i have not kept any check on it
+			String sql="insert  into customer (id,name,email) values (?,?,?)";
+			Object[] args = new Object[] {entityToCreate.getProperty("ID").getValue(),
+							(String)entityToCreate.getProperty("NAME").getValue(),
+							(String)entityToCreate.getProperty("EMAIL").getValue()
+					};
+						
+			this.jdbcTemplate.update(sql, args);
+		    return entityToCreate;
+			
+//		// TODO Auto-generated method stub
+//		// the ID of the newly created product entity is generated automatically
+//	    int newId = 1;
+//	    while (productIdExists(newId)) {
+//	      newId++;
+//	    }
+//
+//	    Property idProperty = entity.getProperty("ID");
+//	    if (idProperty != null) {
+//	      idProperty.setValue(ValueType.PRIMITIVE, Integer.valueOf(newId));
+//	    } else {
+//	      // as of OData v4 spec, the key property can be omitted from the POST request body
+//	      entity.getProperties().add(new Property(null, "ID", ValueType.PRIMITIVE, newId));
+//	    }
+//	    entity.setId(createId("Products", newId));
+//	    this.productList.add(entity);
+//
+//	    return entity;
 	}
 
 }
